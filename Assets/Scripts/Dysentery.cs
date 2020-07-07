1318d883bbcd2601d45fa08f85b39ff892d85547
Matheu Plouffe <matheuplouffe@gmail.com>
2020-07-06 23:12:00 -0500

    Added Dysentery tutorial sample.

---
+++ Assets/Scripts/Dysentery.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+
+public struct Dysentary : IComponentData
+{
+    public float DeathSecondsPoint;         // point in time the player contracted Dystentery
+    public float DeathSecondsDuration;      // the duration it takes the player to die from Dystentery
+}
---
+++ Assets/Scripts/DysenterySystem.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ECS_RTS.DystenterySample
+{
+    public class DysenterySystem : SystemBase
+    {
+        EntityCommandBufferSystem barrier => World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        protected override void OnUpdate()
+        {
+            var currentSeconds = (float)Time.ElapsedTime;
+            var commandBuffer = barrier.CreateCommandBuffer().ToConcurrent();
+
+            Entities
+                .ForEach((Entity entity, int entityInQueryIndex, in PlayerComponent player) =>
+                {
+                    if (player.Hygiene <= 0.6) return;
+
+                    commandBuffer.AddComponent(entityInQueryIndex, entity, new Dysentary
+                    {
+                        DeathSecondsPoint = currentSeconds
+                    });
+                })
+                .WithName("DysentaryContractJob")
+                .ScheduleParallel();
+            
+            barrier.AddJobHandleForProducer(Dependency);
+
+            Entities
+                .WithAny<PlayerComponent>()
+                .ForEach((Entity entity, int entityInQueryIndex, in Dysentary dysentery) =>
+                {
+                    if (currentSeconds - dysentery.DeathSecondsPoint < dysentery.DeathSecondsDuration) return;
+                    commandBuffer.DestroyEntity(entityInQueryIndex, entity);
+                })
+                .WithName("DystentaryDeathJob")
+                .ScheduleParallel();
+
+            barrier.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
