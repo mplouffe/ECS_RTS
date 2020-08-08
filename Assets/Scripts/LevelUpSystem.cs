e4bf6e7e0f9a73fd975ca3f467916323c90acfbe
mplouffe <matheuplouffe@gmail.com>
2020-08-08 11:43:06 -0500

    Restarting project with emphasis on solid ECS fundamentals.

---
+++ Assets/Scripts/LevelUpSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class LevelUpSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        float dt = Time.DeltaTime;
+        Entities.ForEach((ref LevelComponent levelComponent) =>
+        {
+            levelComponent.level += 1f * dt;
+        });
+    }
+}
---
+++ Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MoveSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        float dt = Time.DeltaTime;
+
+        Entities.ForEach((ref Translation translation, ref MoveSpeedComponent moveSpeedComponent) =>
+        {
+            translation.Value.y += moveSpeedComponent.moveSpeed * dt;
+
+            if (translation.Value.y > 5f)
+            {
+                moveSpeedComponent.moveSpeed = -math.abs(moveSpeedComponent.moveSpeed);
+            }
+            
+            if (translation.Value.y < -5f)
+            {
+                moveSpeedComponent.moveSpeed = math.abs(moveSpeedComponent.moveSpeed);
+            }
+        });
+    }
+}
